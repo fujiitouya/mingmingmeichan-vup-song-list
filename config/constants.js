0716698541b91a47f6d936849a9bd5d2edca997b
kazusalove <a6087349@outlook.com>
2023-09-20 15:12:43 +0800

    migrate code
    fyi

---
+++ config/constants.js
@@ -0,0 +1,36 @@
+let config = {
+    Name: "冥冥meichan",
+    
+    BiliLiveRoomID: "22650610",
+    
+    NetEaseMusicId: "",
+    QQMusicId: "",
+    Footer: "Copyright © 2023 冥冥meichan 解释权归本直播间所有。",
+
+    Cursor: true,
+
+    LanguageCategories: ["中文", "日文", "术力口"],
+    RemarkCategories: [],
+
+    BannerTitle: "个人势໒꒱° 每天8点后直播(不准时)！不会忘记每分每秒的感动✨商务合作请加q2302905180",
+    BannerContent: [``,],
+
+    CustomButtons: [
+        // {
+        //     link: "mailto:2302905180@qq.com",
+        //     name: "商务合作",
+        //     image: ""
+
+        // },
+        {
+            link: "https://space.bilibili.com/45502",
+            name: "B站主页",
+            image: ""
+        }
+    ]
+}
+
+
+module.exports = {
+    config
+}
